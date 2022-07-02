536dd1595d8f8654a992fae29c88ad7c2150fe6a
chen_yang <2584623834@qq.com>
2022-07-02 16:24:13 +0800

    xx

---
+++ xxx/appl.c
@@ -0,0 +1,13 @@
+/*
+ * appl.c
+ *
+ *  Created on: Jul 2, 2022
+ *      Author: DH
+ */
+#include "appl.h"
+
+void appl(void)
+{
+	  delay_ms(100);
+	  LED();
+}
---
+++ xxx/appl.h
@@ -0,0 +1,13 @@
+/*
+ * appl.h
+ *
+ *  Created on: Jul 2, 2022
+ *      Author: DH
+ */
+
+#ifndef INC_APPL_H_
+#define INC_APPL_H_
+
+void appl(void);
+
+#endif /* INC_APPL_H_ */
